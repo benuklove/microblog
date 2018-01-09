bddb0905534b41e0a95e74c31a357ce8b42a3f94
benlove <benuklove@gmail.com>
2018-01-08 20:30:51 -0600

    Added mock posts for user post testing

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,14 @@ from app import app
 
 def index():
     user = {'username': 'Ben'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
